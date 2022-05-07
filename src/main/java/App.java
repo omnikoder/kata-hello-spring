495b6e0853e6bb74eaeb46c92efdd43c26e5fc9c
omnikoder <98806400+omnikoder@users.noreply.github.com>
2022-05-07 17:06:28 +0500

    feat: бин Cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,8 +4,17 @@ public class App {
     public static void main(String[] args) {
 
         try (var applicationContext = new AnnotationConfigApplicationContext(AppConfig.class)) {
-            HelloWorld bean = applicationContext.getBean("helloworld", HelloWorld.class);
-            System.out.println(bean.getMessage());
+            var helloBean1 = applicationContext.getBean("helloworld", HelloWorld.class);
+            var helloBean2 = applicationContext.getBean("helloworld", HelloWorld.class);
+            var catBean1 = applicationContext.getBean(Cat.class, "Puma");
+            var catBean2 = applicationContext.getBean(Cat.class, "Lion");
+
+            System.out.printf("\u001b[33m%s\u001b[0m == \u001b[33m%s\u001b[0m: \u001b[32m%b\u001b[0m\n",
+                    helloBean1, helloBean2, helloBean1 == helloBean2);
+            System.out.printf("\u001b[33m%s\u001b[0m %s == \u001b[33m%s\u001b[0m %s: \u001b[31m%b\u001b[0m\n",
+                    catBean1, catBean1.getName(),
+                    catBean2, catBean2.getName(),
+                    catBean1 == catBean2);
         }
     }
 }
